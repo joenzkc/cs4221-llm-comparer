420eba5cb4a4f8c0c68f4180f64faea7e272038c
AntheaJFW <wong.ji.fong@gmail.com>
2024-03-18 21:19:54 +0800

    refactor js into static, move template to base.html and added claude

--- app/llms/claude.py
+++ app/llms/claude.py
@@ -8,7 +8,10 @@ claude_client = anthropic.Anthropic(api_key=api_key)
 
 base_prompt = "You are an SQL Expert. You are asked to write SQL queries based on the prompts given to you. Your response should be a valid SQL query."
 
-def generate_claude_response(prompt):
+def generate_claude_response(prompt: str, content_prompt: str='') -> str:
+    if content_prompt:
+        base_prompt = content_prompt
+    print(f'claude content prompt: {content_prompt} | claude prompt: {prompt}')
     messages = [{"role": "user", "content": prompt}]
     response = claude_client.messages.create(model="claude-3-opus-20240229", max_tokens=300, system=base_prompt, messages=messages)
     return response.content[0].text
--- app/llms/openai.py
+++ app/llms/openai.py
@@ -8,10 +8,26 @@ api_key = os.getenv('OPENAI_API_KEY')  # Use getenv instead of environ.get
 client = OpenAI(api_key=api_key)
 
 # Setup base prompt
-base_prompt = {"role": "system", "content": "You are an SQL Expert. You are asked to write SQL queries based on the prompts given to you. Your response should be a valid SQL query."}
+base_prompt = {
+    "role": "system", 
+    "content": ("You are an SQL Expert. You are asked to write SQL"
+                "queries based on the prompts given to you. Your "
+                "response should be a valid SQL query.")
+}
 
-def generate_ai_response(prompt):
-    messages = [base_prompt]
+def generate_ai_response(prompt: str, content_prompt: str='') -> str:
+    """
+    args:
+        prompt (str): String to send to openai endpoint
+        content_prompt (str, optional): Custom base prompt content
+    returns:
+        str: Response in markdown from AI
+    """
+    messages = [{
+        'role': 'system',
+        'content': content_prompt
+    } if content_prompt else base_prompt]
+    print(f'openai content prompt: {content_prompt} | openai prompt: {prompt}')
     messages.append({"role": "user", "content": prompt})
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=messages)  # Use ChatCompletion.create
     return response.choices[0].message.content
